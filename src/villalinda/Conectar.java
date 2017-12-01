887104dbe00e02763b1d8c88cfde6eabfdb1d598
anonimolopez <jose_lopez_emo@hotmail.com>
2017-11-30 21:32:08 -0600

    Inicia el projecto

---
+++ src/villalinda/Conectar.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package villalinda;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author flavioantonio
+ */ 
+public class Conectar {
+   // private static String dba = "unalm";
+private static String user = "root";
+    private static String pass = "";
+    private static String url = "jdbc:mysql://localhost:3306/villalida";
+    private static Connection conn;
+    private static Connection x;
+   
+    public static Connection geConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn=DriverManager.getConnection(url, user, pass);
+            //conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/villalida","root","");
+            System.out.println("exito");
+        } catch (Exception e) {
+             JOptionPane.showMessageDialog(null," Error En La Base De Datos " + e);
+
+        }
+        return conn;
+    }
+    
+    public static void main(String[] args) {
+           geConnection();
+           
+        
+    }
+}
